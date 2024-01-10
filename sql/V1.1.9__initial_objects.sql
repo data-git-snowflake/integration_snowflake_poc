ba6cb71cd4aa3a40e18bf8483c101143ef548252
Fvargas <fvarrom@gmail.com>
2024-01-10 15:05:55 -0500

    add currency column to user table

---
+++ sql/V1.1.9__initial_objects.sql
@@ -0,0 +1,12 @@
+USE DATABASE TEST_CI_CD;
+USE SCHEMA {{env_name}};
+CREATE OR REPLACE TABLE USERS_TEST
+(
+   FIRST_NAME VARCHAR
+  ,LAST_NAME VARCHAR
+  ,ALIAS VARCHAR
+);
+
+alter table USERS_TEST add column AGE varchar;
+alter table USERS_TEST add column COUNTRY_NAME varchar;
+alter table USERS_TEST add column CURRENCY varchar;
